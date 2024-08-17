02835f6528dbc1b494c9ab5c579dbb4e0f9dbb89
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-17 17:40:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import WeatherBox from './component/WeatherBox';
+import WeatherButton from './component/WeatherButton';
 
+// 1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
+// 2. 날씨정보에는 도시, 섭씨, 화씨 날씨 상태 들어간다.
+// 3. 5개의 버튼이 있다.(1개는 현재위치, 4개는 다른도시)
+// 4. 도시버튼 누를떄다마 도시별 날씨가 보인다.
+// 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 function App() {
+  const [weather, setWeather] = useState(null);
+  // 배열에 값을 안주면 렌더 후에 바로 실행
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log('현재위치:', lat, lon);
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=861351e4b65eb1adfebe021f96f31ff3&units=metric`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log('data:', data);
+    setWeather(data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <WeatherBox weather={weather} />
+        <WeatherButton />
+      </div>
     </div>
   );
 }
